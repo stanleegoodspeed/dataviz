58b334537d887a5fb59c5d690635a5ad6175f990
stanleegoodspeed <ccole3390@gmail.com>
2021-04-14 11:12:36 -0400

    reorg

--- scrolly/js/chart-builder.js
+++ scrolly/js/chart-builder.js
@@ -126,6 +126,48 @@ d3.csv("https://raw.githubusercontent.com/stanleegoodspeed/dataviz/main/data/dat
       .attr("cx", function (d) { return x(d.MinIncome); } )
       .attr("cy", function (d) { return y(d.NumHouseholds); } )
       .attr("r", 3.5)
-      .style("fill", "#ff0652");  
+      .style("fill", "#c600ff");  
+
+});
+
+/*** CHART 4 ***/
+
+// append the svg object to the body of the page
+var svg2 = d3.select("#my_dataviz_2")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("https://raw.githubusercontent.com/stanleegoodspeed/dataviz/main/data/data-v2.csv", function(data) {
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([60000, 75000])
+    .range([ 0, width ]);
+  svg2.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([50000000, 60000000])
+    .range([ height, 0]);
+  svg2.append("g")
+    .call(d3.axisLeft(y));
+
+  // Add dots
+  svg2.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.MinIncome); } )
+      .attr("cy", function (d) { return y(d.NumHouseholds); } )
+      .attr("r", 3.5)
+      .style("fill", "#ffb600");  
 
 });
